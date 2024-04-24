6378d5709ca7001ae9e50ee48295c1f71ca15a92
brooklynb7 <brooklynb7@gmail.com>
2024-04-24 21:25:21 +0800

    Update s4 for 10.2.6

--- MeetingStone/MenuTableAPI.lua
+++ MeetingStone/MenuTableAPI.lua
@@ -401,12 +401,12 @@ function ListOfDungeons(menuType)
     -- local Activitys = { 1164, 1168, 1172, 1188, 518, 507, 462, 1195 }
 
     -- S3
-    local Dungeons = { 11, 54, 113, 118, 137, 145, 316, 317 }
-    local Activitys = { 184, 1274, 460, 463, 502, 530, 1247, 1248 }
+    -- local Dungeons = { 11, 54, 113, 118, 137, 145, 316, 317 }
+    -- local Activitys = { 184, 1274, 460, 463, 502, 530, 1247, 1248 }
 
     -- S4
-    -- local Dungeons = { 302,303,304,305,306,307,308,309 }
-    -- local Activitys = { 1160, 1164, 1168, 1172, 1176, 1180, 1184, 1188 }
+    local Dungeons = { 302,303,304,305,306,307,308,309 }
+    local Activitys = { 1160, 1164, 1168, 1172, 1176, 1180, 1184, 1188 }
 
     -- C_MythicPlus.IsMythicPlusActive()
     -- /run for i=500,600 do local info = C_LFGList.GetActivityInfoTable(i); if info then print(i, info.fullName) end end
